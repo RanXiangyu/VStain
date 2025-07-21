f188d7f9a19da593ea8ce8b892d9de86bf2d0371
RanXiangyu <2375411682@qq.com>
2025-07-21 07:45:32 +0000

    test1

---
+++ styleid_package.py
@@ -0,0 +1,93 @@
+import argparse, os #解析命令行参数
+import torch
+import numpy as np
+from omegaconf import OmegaConf  # 加载模型配置文件
+from PIL import Image
+from einops import rearrange
+from pytorch_lightning import seed_everything #设置随机种子
+from torch import autocast
+from contextlib import nullcontext
+import copy
+
+from ldm.util import instantiate_from_config
+from ldm.models.diffusion.ddim import DDIMSampler
+
+import torchvision.transforms as transforms
+import torch.nn.functional as F
+import time
+import pickle
+
+
+# test
+def save_img_from_sample(model, samples_ddim, fname):
+    x_samples_ddim = model.decode_first_stage(samples_ddim)
+    x_samåples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
+    x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
+    x_image_torch = torch.from_numpy(x_samples_ddim).permute(0, 3, 1, 2)
+    x_sample = 255. * rearrange(x_image_torch[0].cpu().numpy(), 'c h w -> h w c')
+    img = Image.fromarray(x_sample.astype(np.uint8))
+    img.save(fname)
+
+def feat_merge(opt, cnt_feats, sty_feats, start_step=0):
+    feat_maps = [{'config': {
+                'gamma':opt.gamma,
+                'T':opt.T,
+                'timestep':_,
+                }} for _ in range(50)]
+
+    for i in range(len(feat_maps)):
+        if i < (50 - start_step):
+            continue
+        cnt_feat = cnt_feats[i]
+        sty_feat = sty_feats[i]
+        ori_keys = sty_feat.keys()
+
+        for ori_key in ori_keys:
+            if ori_key[-1] == 'q':
+                feat_maps[i][ori_key] = cnt_feat[ori_key]
+            if ori_key[-1] == 'k' or ori_key[-1] == 'v':
+                feat_maps[i][ori_key] = sty_feat[ori_key]
+    return feat_maps
+
+
+# 定义加载图片函数
+def load_img(path):
+    image = Image.open(path).convert("RGB")
+    x, y = image.size
+    print(f"Loaded input image of size ({x}, {y}) from {path}")
+    h = w = 512
+    image = transforms.CenterCrop(min(x,y))(image)
+    image = image.resize((w, h), resample=Image.Resampling.LANCZOS)
+    image = np.array(image).astype(np.float32) / 255.0
+    image = image[None].transpose(0, 3, 1, 2)
+    image = torch.from_numpy(image)
+    return 2.*image - 1.
+
+# 定义AdaIN函数 自适应实例归一化
+def adain(cnt_feat, sty_feat):
+    cnt_mean = cnt_feat.mean(dim=[0, 2, 3],keepdim=True)
+    cnt_std = cnt_feat.std(dim=[0, 2, 3],keepdim=True)
+    sty_mean = sty_feat.mean(dim=[0, 2, 3],keepdim=True)
+    sty_std = sty_feat.std(dim=[0, 2, 3],keepdim=True)
+    output = ((cnt_feat-cnt_mean)/cnt_std)*sty_std + sty_mean
+    return output
+
+# 模型加载函数
+def load_model_from_config(config, ckpt, verbose=False):
+    print(f"Loading model from {ckpt}")
+    pl_sd = torch.load(ckpt, map_location="cpu")
+    if "global_step" in pl_sd:
+        print(f"Global Step: {pl_sd['global_step']}")
+    sd = pl_sd["state_dict"]
+    model = instantiate_from_config(config.model)
+    m, u = model.load_state_dict(sd, strict=False)
+    if len(m) > 0 and verbose:
+        print("missing keys:")
+        print(m)
+    if len(u) > 0 and verbose:
+        print("unexpected keys:")
+        print(u)
+
+    model.cuda()
+    model.eval()
+    return model
